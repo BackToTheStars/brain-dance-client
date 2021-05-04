9b5ec40cd704b8f9b24dfafc0a8bba984ccee5c1
BackToTheStars <nikgrigoryev@gmail.com>
2021-05-03 21:33:43 -0700

    refactoring turns as react components 10

--- components/popups/AddEditTurnPopup.js
+++ components/popups/AddEditTurnPopup.js
@@ -2,10 +2,75 @@ import { useEffect, useState } from 'react';
 import { getQuill } from '../helpers/quillHandler';
 import { useUiContext } from '../contexts/UI_Context';
 
+const TEMPLATE_PICTURE = 1;
+const TEMPLATE_VIDEO = 2;
+const TEMPLATE_COMMENT = 3;
+const TEMPLATE_MIXED = 4;
+const TEMPLATE_PDF = 5;
+const TEMPLATE_AUDIO = 6;
+const TEMPLATE_CAROUSEL = 7;
+const TEMPLATE_NEWS = 8;
+
+const FIELD_HEADER = 1;
+const FIELD_PICTURE = 2;
+const FIELD_VIDEO = 3;
+const FIELD_DATE = 4;
+const FIELD_SOURCE = 5;
+
+const settings = {
+  [TEMPLATE_PICTURE]: {
+    availableFields: [FIELD_PICTURE],
+    value: 'picture',
+    label: 'Text / picture',
+  },
+  [TEMPLATE_VIDEO]: {
+    availableFields: [FIELD_VIDEO],
+    value: 'video',
+    label: 'Text / video',
+  },
+  [TEMPLATE_COMMENT]: {
+    availableFields: [],
+    value: 'comment',
+    label: 'Comment',
+  },
+};
+
+const templatesToShow = [TEMPLATE_PICTURE, TEMPLATE_VIDEO, TEMPLATE_COMMENT];
+
+const fieldSettings = {
+  [FIELD_HEADER]: {
+    label: 'Header',
+    prefixClass: 'header',
+  },
+  [FIELD_PICTURE]: {
+    label: 'Image Url',
+    prefixClass: 'image-url',
+    special: true,
+  },
+  [FIELD_VIDEO]: {
+    label: 'Video Url',
+    prefixClass: 'video-url',
+    special: true,
+  },
+  [FIELD_DATE]: {
+    label: 'Date',
+    prefixClass: 'date',
+    inputType: 'date',
+  },
+  [FIELD_SOURCE]: {
+    label: 'Source Url',
+    prefixClass: 'source-url',
+  },
+};
+
+const fieldsToShow = Object.keys(fieldSettings); // возвращает массив строк-ключей объекта
+
 const AddEditTurnPopup = () => {
   // https://transform.tools/html-to-jsx   - преобразователь HTML в JSX
 
   const [quillConstants, setQuillConstants] = useState({}); // { quill, getQuillTextArr }
+  const [activeTemplate, setActiveTemplate] = useState(TEMPLATE_PICTURE);
+  const availableFields = settings[activeTemplate].availableFields;
 
   const {
     createEditTurnPopupIsHidden,
@@ -48,31 +113,46 @@ const AddEditTurnPopup = () => {
           </div>
           <div className="col-4">
             <div className="radio-group">
-              <div className="form-group row">
-                <input
-                  type="radio"
-                  name="type"
-                  defaultValue="picture"
-                  defaultChecked
-                />
-                <span>Text / picture</span>
-              </div>
-              <div className="form-group row">
-                <input type="radio" name="type" defaultValue="video" />
-                <span>Text / video</span>
-              </div>
-              <div className="form-group row">
-                <input type="radio" name="type" defaultValue="comment" />
-                <span>Comment</span>
-              </div>
+              {templatesToShow.map((el) => {
+                const templateSettings = settings[el];
+                return (
+                  <div className="form-group row">
+                    <input
+                      type="radio"
+                      name="template"
+                      value={templateSettings.value}
+                      defaultChecked={activeTemplate === el}
+                    />
+                    <span>{templateSettings.label}</span>
+                  </div>
+                );
+              })}
             </div>
             <input type="hidden" id="idInput" />
 
-            <FormInput label="Header" prefixClass="header" />
+            {fieldsToShow
+              .filter((field) => {
+                if (!fieldSettings[field].special) {
+                  return true;
+                }
+                return availableFields.includes(parseInt(field));
+              })
+              .map((field) => {
+                return (
+                  <FormInput
+                    label={fieldSettings[field].label}
+                    prefixClass={fieldSettings[field].prefixClass}
+                    inputType={fieldSettings[field].inputType}
+                  />
+                );
+              })}
+
+            {/* <FormInput label="Header" prefixClass="header" />
             <FormInput label="Date" prefixClass="date" inputType="date" />
             <FormInput label="Source Url" prefixClass="source-url" />
+
             <FormInput label="Image Url" prefixClass="image-url" />
-            <FormInput label="Video Url" prefixClass="video-url" />
+            <FormInput label="Video Url" prefixClass="video-url" /> */}
           </div>
         </div>
         <div className="row mb-4">
