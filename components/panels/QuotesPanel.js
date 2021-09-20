84ab4ae5b948db9e1d14159058151c53a59ece6f
BackToTheStars <nikgrigoryev@gmail.com>
2021-09-19 22:52:27 -0700

    Refactoring to React 71 - request those turns & lines out of view

--- components/panels/QuotesPanel.js
+++ components/panels/QuotesPanel.js
@@ -1,6 +1,7 @@
 import { useTurnContext, ACTION_LINES_DELETE } from '../contexts/TurnContext';
 import { RULE_TURNS_CRUD } from '../config';
 import { useUserContext } from '../contexts/UserContext';
+import { useEffect, useState } from 'react';
 
 const cutTextToSize = (text, size) => {
   console.log(text, size);
@@ -11,8 +12,14 @@ const cutTextToSize = (text, size) => {
 const QuotesPanel = () => {
   const { dispatch, activeQuote, lineEnds, quotesInfo, deleteLines } =
     useTurnContext();
-  const { can } = useUserContext();
-  if (!activeQuote) return null; // прочитать о разнице с false
+  const [quotesOutOfScreenInfo, setQuotesOutOfScreenInfo] = useState({});
+  const [preparedLines, setPreparedLines] = useState([]);
+  const {
+    can,
+    request,
+    info: { hash },
+  } = useUserContext();
+  // if (!activeQuote) return null; // прочитать о разнице с false
 
   const handleDelete = (e, _id) => {
     e.preventDefault();
@@ -28,16 +35,82 @@ const QuotesPanel = () => {
     }
   };
 
-  const clickedQuoteInfo =
-    lineEnds[`${activeQuote.turnId}_${activeQuote.quoteId}`];
-  const lines = clickedQuoteInfo ? clickedQuoteInfo.lines : [];
+  useEffect(() => {
+    if (!activeQuote) setQuotesOutOfScreenInfo({});
+
+    const clickedQuoteInfo = !!activeQuote
+      ? lineEnds[`${activeQuote.turnId}_${activeQuote.quoteId}`]
+      : null;
+    const lines = clickedQuoteInfo ? clickedQuoteInfo.lines : [];
+    const preparedLines = lines.map((line) => {
+      let turnIdOutOfScreen = null;
+      let sourceQuoteInfo = {};
+      let targetQuoteInfo = {};
+      if (!!quotesInfo[line.sourceTurnId]) {
+        sourceQuoteInfo = quotesInfo[line.sourceTurnId].find(
+          (quoteInfo) => line.sourceMarker === quoteInfo.quoteId
+        );
+      } else {
+        turnIdOutOfScreen = line.sourceTurnId;
+      }
+
+      if (!!quotesInfo[line.targetTurnId]) {
+        targetQuoteInfo = quotesInfo[line.targetTurnId].find(
+          (quoteInfo) => line.targetMarker === quoteInfo.quoteId
+        );
+      } else {
+        turnIdOutOfScreen = line.targetTurnId;
+      }
+
+      return { ...line, sourceQuoteInfo, targetQuoteInfo, turnIdOutOfScreen };
+    });
+    setPreparedLines(preparedLines);
+    const turnIdsOutOfScreen = preparedLines
+      .filter((line) => !!line.turnIdOutOfScreen)
+      .map((line) => line.turnIdOutOfScreen);
+
+    if (!!turnIdsOutOfScreen.length) {
+      request(
+        `turns?hash=${hash}&turnIds=${turnIdsOutOfScreen.join(',')}`
+      ).then((data) => {
+        console.log(data);
+        setPreparedLines(
+          preparedLines.map((line) => {
+            if (!!line.turnIdOutOfScreen) {
+              // загружаем цитаты хода который не видно в области видимости
+              debugger;
+              const turnOutOfScreen = data.items.find(
+                (turn) => turn._id === line.turnIdOutOfScreen
+              );
+              if (!Object.keys(line.sourceQuoteInfo).length) {
+                return {
+                  ...line,
+                  sourceQuoteInfo: turnOutOfScreen.quotes.find(
+                    (quote) => line.sourceMarker === quote.quoteId
+                  ),
+                };
+              }
+              if (!Object.keys(line.targetQuoteInfo).length) {
+                return {
+                  ...line,
+                  targetQuoteInfo: turnOutOfScreen.quotes.find(
+                    (quote) => line.targetMarker === quote.quoteId
+                  ),
+                };
+              }
+            } else return line;
+          })
+        );
+      });
+    }
+    console.log(turnIdsOutOfScreen);
+  }, [activeQuote]);
 
   return (
-    <div className="quotes-panel panel">
-      {/* <div>
-        {activeQuote.turnId} {activeQuote.quoteId}
-      </div> */}
-      {!!lines.length && (
+    <div
+      className={`${!preparedLines.length ? 'hidden' : ''} quotes-panel panel`}
+    >
+      {!!preparedLines.length && (
         <table className="table m-0 table-dark table-striped">
           <thead>
             <tr>
@@ -49,24 +122,21 @@ const QuotesPanel = () => {
             </tr>
           </thead>
           <tbody>
-            {lines.map((line, index) => {
-              const sourceQuoteInfo = quotesInfo[line.sourceTurnId].find(
-                (quoteInfo) => line.sourceMarker === quoteInfo.quoteId
-              );
-              const targetQuoteInfo = quotesInfo[line.targetTurnId].find(
-                (quoteInfo) => line.targetMarker === quoteInfo.quoteId
-              );
+            {preparedLines.map((line, index) => {
+              const { sourceQuoteInfo, targetQuoteInfo } = line;
+
               return (
                 <tr key={index}>
                   <td>{line.author}</td>
                   <td>{line.type}</td>
                   <td>
                     {!!sourceQuoteInfo &&
-                      cutTextToSize(sourceQuoteInfo.text, 20)}
+                      cutTextToSize(sourceQuoteInfo.text || '', 22)}
                   </td>
                   <td>
                     {!!targetQuoteInfo &&
-                      cutTextToSize(targetQuoteInfo.text, 20)}
+                      cutTextToSize(targetQuoteInfo.text || '', 22)}
+                    {/* сделать резиновую обрезку текста */}
                   </td>
                   {can(RULE_TURNS_CRUD) && (
                     <td className="text-right">
