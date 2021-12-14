9fa40e2048489f30fe2470af0e9f6fb49cc5cb39
BackToTheStars <nikgrigoryev@gmail.com>
2021-12-13 21:22:27 -0800

    Turn compression to quotes 8 - splicing paragraph pieces

--- components/turn/Compressor.js
+++ components/turn/Compressor.js
@@ -1,8 +1,11 @@
 import { Fragment, useRef } from 'react';
 import { useEffect } from 'react/cjs/react.development';
 
-const Compressor = ({ width, paragraph, textPieces }) => {
+const Compressor = ({ width, paragraph: originalParagraph, textPieces }) => {
   //
+  const paragraph = originalParagraph.map((paragraphItem) => ({
+    ...paragraphItem,
+  }));
   const wrapperRef = useRef(null); // @learn null это мы, undefined, это система
 
   // @fixme: цитата делит одно слово на несколько слов
@@ -41,6 +44,54 @@ const Compressor = ({ width, paragraph, textPieces }) => {
     }
     console.log(textPieces);
     console.log(paragraph);
+
+    let paragraphCountingBuffer = [];
+    lettersCount = 0;
+    let paragraphIndex = 0;
+
+    for (let i = 0; i < textPieces.length - 1; i += 1) {
+      const { startLettersCount } = textPieces[i + 1];
+
+      for (let j = paragraphIndex; j < paragraph.length; j += 1) {
+        const insertLength = paragraph[j].insert.length;
+        if (insertLength + lettersCount === startLettersCount) {
+          console.log('if 1');
+          // решить, делать ли копию
+          paragraphCountingBuffer.push(paragraph[j]);
+          paragraphIndex = j + 1;
+          textPieces[i].paragraph = paragraphCountingBuffer;
+          paragraphCountingBuffer = [];
+          lettersCount += insertLength;
+          break; // @learn - breaks работает для while, for, switch
+          //
+        } else if (insertLength + lettersCount < startLettersCount) {
+          console.log('if 2', insertLength + lettersCount, startLettersCount);
+          paragraphCountingBuffer.push(paragraph[j]);
+          lettersCount += insertLength;
+          //
+        } else if (insertLength + lettersCount > startLettersCount) {
+          console.log('if 3', insertLength + lettersCount, startLettersCount);
+          const difference = insertLength + lettersCount - startLettersCount;
+          paragraphCountingBuffer.push(
+            paragraph[j].insert.slice(0, difference)
+          );
+          paragraph.splice(j + 1, 0, {
+            insert: paragraph[j].insert.slice(difference),
+          });
+          paragraph[j].insert = paragraph[j].insert.slice(0, difference);
+          paragraphIndex = j + 1;
+          textPieces[i].paragraph = paragraphCountingBuffer;
+          paragraphCountingBuffer = [];
+          // lettersCount -= insertLength;
+          lettersCount += difference;
+          break;
+          //
+        }
+      }
+    }
+    console.log('завершение цикла');
+    textPieces[textPieces.length - 1].paragraph = paragraphCountingBuffer;
+    console.log({ textPieces });
   }, [width]);
 
   return (
